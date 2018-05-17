d44b73adf33187a35f5f238d70b09c4fe7c67899
Pablo <pplazam@salesianospizarrales.com>
2018-05-18 00:06:05 +0200

    Added decrypter

--- ironHackers-Decrypter.sh
+++ ironHackers-Decrypter.sh
@@ -1,32 +1,25 @@
+#!/bin/bash
 function helps {
-echo "File crypter  -  help"
-            echo "-f file to encrypt"
-	    echo "Crypter need a file to work"
+echo "File decrypter  -  help"
+echo "This only works for IronHackers - Crypter"
+            echo "-f file to decrypt"
+	    echo "-p file with pass.enc"
+	    echo "-k PEM key file"
+	    echo "Decrypter need a file to work"
+	    echo " "
+	    echo "Example of usage: ./ironHackers-Decrypter.sh -f file.txt.WNCRY -p key.enc -k key.pem"
             exit 1       
 
+
 }
-function crypt {
-if [ -f $1 ]; then
-echo "$1 file selected"
-else
-echo "File not found"
-exit
-fi
-mkdir keys
-openssl genrsa -out key.pem 2048 
-openssl rsa -in key.pem -out key.pub.pem -outform PEM -pubout 
-openssl rand -base64 48 > key.txt
-openssl enc -aes-256-cbc -pass file:key.txt -in "$1" -out "$1.WNCRY"
-openssl rsautl -encrypt -in key.txt -out key.enc -inkey key.pub.pem -pubin
+function decrypt {
+original=$(echo "$1" | cut -d "." -f 1)
+openssl rsautl -decrypt -inkey $3 -in $2 -out key.txt
+openssl enc -aes-256-cbc -d -pass file:key.txt -in "$1" -out "$original.decrypted"
 rm key.txt
 rm $1
-mv key.enc keys/key.enc
-mv key.pem keys/key.pem
-rm key.pub.pem
-echo "Crypted File $1"> @Please_Read_Me@.txt
-echo "Finished.....Keys stored in /keys"
+echo "File $1 decrypted! Decrypte file is $original.decrypted"
 }
-echo ""
 echo ""
 echo "
 
@@ -41,16 +34,29 @@ By Ghostpp7 @ IronHackers.es
 "
 echo ""
 echo ""
+if [ $# == 6 ]; then
+cont=0
+for num in $@; do
+array[$cont]=$num
+cont=$(expr $cont + 1)
 
-if [ $# == 2 ]; then
-
-if [ "$1" == "-f" ]; then
-crypt $2
-else
+done
+for((i=0;i<$#;i++)); do
+if [ "${array[$i]}" == "-f" ]; then
+i=$(expr $i + 1)
+file=${array[$i]}
+elif [ "${array[$i]}" == "-p" ]; then
+i=$(expr $i + 1)
+pass=${array[$i]}
+elif [ "${array[$i]}" == "-k" ]; then
+i=$(expr $i + 1)
+key=${array[$i]}
+elif [ $i == 1 ] || [ $i == 3 ] || [ $i == 5 ]; then
 helps
+exit
 fi
-
+done
+decrypt $file $pass $key
 else
 helps
-   
 fi
